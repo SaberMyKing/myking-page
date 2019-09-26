357c90f81f7fbdce469d24560715db45b52ad981
SaberMyKing <liangxinleicn@gmail.com>
2019-09-26 23:30:50 +0800

    add component banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../stylesheets/Common.css';
+
+class Banner extends React.Component {
+    render() {
+        return (
+            <div class='banner-box'>
+                <div class='banner'>
+                    {/* <img src='/myface.jpg' class='navigation-myface' alt='This is SaberMyKing.'></img> */}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Banner;
--- src/components/pages/IndexPage.jsx
+++ src/components/pages/IndexPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavigationBar from '../NavigationBar'
+import Banner from '../Banner'
 import '../../stylesheets/Page.css'
 class IndexPage extends React.Component {
     render() {
         return (
             <div class='index-page'>
                 <NavigationBar />
+                <Banner />
             </div>
         );
     }
