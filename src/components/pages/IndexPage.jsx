5eb3d91e7155e869da1ec2c10f00b9b482436284
SaberMyKing <liangxinleicn@gmail.com>
2019-09-27 00:11:25 +0800

    add component github corner

--- src/components/pages/IndexPage.jsx
+++ src/components/pages/IndexPage.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import NavigationBar from '../NavigationBar'
 import Banner from '../Banner'
 import '../../stylesheets/Page.css'
+import GithubCorner from 'react-github-corner';
+
 class IndexPage extends React.Component {
     render() {
         return (
             <div class='index-page'>
+                <GithubCorner href='https://github.com/sabermyking/myking-page' />
                 <NavigationBar />
                 <Banner />
             </div>
