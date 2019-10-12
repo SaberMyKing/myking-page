1d6e88859355e700d1a4f8ca176c6700699f3be6
SaberMyKing <liangxinleicn@gmail.com>
2019-10-12 17:31:03 +0800

    use the navigation bar in common in each pages

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import IndexPage from './pages/IndexPage';
 import ArticlePage from './pages/ArticlePage';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import NavigationBar from './NavigationBar'
+import GithubCorner from 'react-github-corner';
 
 function AppRouter() {
     return (
         <Router>
-            <Route path = "/" component = { IndexPage } />
-            <Route path = "/articles" component = { ArticlePage } />
-            <Route path = "/home" component = { HomePage } />
-            <Route path = "/about" component = { AboutPage } />
+            <GithubCorner href='https://github.com/sabermyking/myking-page' />
+            <NavigationBar />
+
+            <Switch>
+                <Route path = "/" component = { IndexPage } />
+                <Route path = "/articles" component = { ArticlePage } />
+                <Route path = "/home" component = { HomePage } />
+                <Route path = "/about" component = { AboutPage } />
+            </Switch>
         </Router>
     );
 }
