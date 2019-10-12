bfa6812dc13b2c58942fe071b3173bf4611e9710
SaberMyKing <liangxinleicn@gmail.com>
2019-10-12 17:04:12 +0800

    replace tag <a> with <Link> in navigation bar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import '../stylesheets/NavigationBar.css';
 
 class NavigationBar extends React.Component {
@@ -7,15 +8,17 @@ class NavigationBar extends React.Component {
             <div class='navigation-bar-box'>
                 <div class='navigation-bar'>
                     <div class='navigation-left-box'>
-                        <div class='navigation-item'><a href='/'>茵蒂克丝</a></div>
-                        <div class='navigation-item'><a href='/articles'>菲特</a></div>
-                        <div class='navigation-item'><a href='/articles'>阿尼妹</a></div>
+                        <div class='navigation-item'><Link to='/'>茵蒂克丝</Link></div>
+                        <div class='navigation-item'><Link to='/articles'>菲特</Link></div>
+                        <div class='navigation-item'><Link to='/articles'>阿尼妹</Link></div>
                     </div>
 
                     <div class='navigation-right-box'>
-                        <div class='navigation-item'><a href='/about'>
+                        <div class='navigation-item'>
+                            <Link to='/about'>
                             <img src='/myface.jpg' class='navigation-myface' alt='This is SaberMyKing.'></img>
-                        </a></div>
+                            </Link>
+                        </div>
                         <div class='navigation-item'><span>路人模式</span></div>
                     </div>
                 </div>
