04d1539229b6132a552d0947782a1dad4fee41cf
SaberMyKing <liangxinleicn@gmail.com>
2019-10-17 13:08:45 +0800

    add react-markdown to article component

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import { Remarkable } from 'remarkable';
+import ReactMarkdown from 'react-markdown';
 
 class Article extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { markdown: "- 简单有效，无需额外的后端存储，开箱即用；\n" +
-        "- 相对与其他存储方式，它很快，只需解密，无 IO 请求；\n" +
-        "- 因为 Session 加密存储在用户浏览器，所以不回因为后端存储丢失而导致 Session 失效；\n" +
-        "- 我们依然可以通过改变 secure_key_base 的方式来使之前的 Session 失效；" }
+        this.state = {}
     }
-
-    getRawMarkup() {
-        let markdown = new Remarkable();
-        return { __html: markdown.render(this.state.markdown) };
+    
+    componentDidMount() {
+        fetch(`/articles/${this.props.id}.md`)
+            .then((response) => response.text())
+            .then((text) => this.setState({ text: text }))
     }
 
     render() {
         return (
-            <div>
-                <div className="content" 
-                    dangerouslySetInnerHTML={ this.getRawMarkup() } />
+            <div className='markdown'>
+                <ReactMarkdown source={ this.state.text } />
             </div>
         )
     }
