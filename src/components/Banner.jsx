363a579fb5f527a90b9ecf53930d3af4b3f7ef3b
SaberMyKing <liangxinleicn@gmail.com>
2019-10-21 02:33:34 +0800

    upload banner image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,10 +4,8 @@ import '../stylesheets/Common.css';
 class Banner extends React.Component {
     render() {
         return (
-            <div className='banner-box'>
-                <div className='banner'>
-                    {/* <img src='/myface.jpg' class='navigation-myface' alt='This is SaberMyKing.'></img> */}
-                </div>
+            <div className='banner-container'>
+                <img src='/banner.png' className='banner'></img>
             </div>
         );
     }
