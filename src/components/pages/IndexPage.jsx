0740c105fc7855fb1054be7f7cecf10ac0cf0d0c
SaberMyKing <liangxinleicn@gmail.com>
2019-10-27 17:21:51 +0800

    update markdown articles

--- src/components/pages/IndexPage.jsx
+++ src/components/pages/IndexPage.jsx
@@ -9,7 +9,7 @@ class IndexPage extends React.Component {
         this.state = {
             article_items: [
                 { title: '骑士王的荣耀', content: '2000年前的英格兰，一位名叫【阿莉斯托蕾雅】的16岁少女默默的走来...', img_src: '/fate.jpg' },
-                { title: '红宝石', content: 'Ruby on Rails...', img_src: '/myface.jpg' },
+                { title: '红宝石', content: '起源于日本，发扬于美国...', img_src: '/ruby.jpg' },
             ]
         }
     }
