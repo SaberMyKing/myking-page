73fe98ac4273a85c0089a0550b8c256abf117e59
SaberMyKing <liangxinleicn@gmail.com>
2019-10-27 19:23:56 +0800

    add about me page

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -7,16 +7,20 @@ class Article extends React.Component {
         super(props);
         this.state = {}
     }
-    
-    componentDidMount() {
+
+    fetchMarkdown() {
         fetch(`/articles/${this.props.id}.md`)
             .then((response) => response.text())
             .then((text) => this.setState({ text: text }))
     }
+    
+    componentDidMount() {
+        this.fetchMarkdown()
+    }
 
     render() {
         return (
-            <ReactMarkdown className="article markdown" source={ this.state.text } />
+            <ReactMarkdown className="markdown" source={ this.state.text } />
         )
     }
 }
--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import '../../stylesheets/Page.css';
+import '../../stylesheets/navigation-bar.css';
+import Article from '../Article';
 
 class AboutPage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
     render() {
         return (
-            <div>
-                Hi, I am SaberMyKing.
+            <div class='about-me-container'>
+                <div class='about-me'>
+                    <img class='about-me-img' src='/myface.jpg'></img>
+                    <div class='about-me-description'>
+                        <Article id='关于我'/>
+                    </div>
+                </div>
             </div>
         );
     }
