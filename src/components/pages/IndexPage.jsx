e5156ab3f89ee70f31fc4a4ee53259d859561df7
SaberMyKing <liangxinleicn@gmail.com>
2019-11-06 02:32:13 +0800

    update articles

--- src/components/pages/IndexPage.jsx
+++ src/components/pages/IndexPage.jsx
@@ -8,6 +8,7 @@ class IndexPage extends React.Component {
         super(props);
         this.state = {
             article_items: [
+                { title: '新海诚的天气之子', content: '小评一下新海诚...', cover_img: '/your_name.jpg' },
                 { title: '骑士王的荣耀', content: '2000年前的英格兰，一位名叫【阿莉斯托蕾雅】的16岁少女默默的走来...', cover_img: '/fate.jpg' },
                 { title: '红宝石', content: '起源于日本，发扬于美国...', cover_img: '/ruby.jpg' },
             ]
